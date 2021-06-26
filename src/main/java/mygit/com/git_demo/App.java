3494f595a9ae0917eca5cebf5f94bcf4348a5bf1
RamanaGithub <adari.vraman@gmail.com>
2021-06-26 21:55:33 +0530

    git-demo files are committed

---
+++ src/main/java/mygit/com/git_demo/App.java
@@ -0,0 +1,16 @@
+package mygit.com.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void addData(String data) {
+		System.out.println("the data is"+data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
