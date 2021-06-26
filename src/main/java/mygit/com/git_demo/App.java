a89555bfe792af9959b11f4e865276d79e3f674a
RamanaGithub <adari.vraman@gmail.com>
2021-06-26 22:20:25 +0530

    if condition added in git-demo project

--- src/main/java/mygit/com/git_demo/App.java
+++ src/main/java/mygit/com/git_demo/App.java
@@ -5,9 +5,12 @@ package mygit.com.git_demo;
  *
  */
 public class App 
-{
+{    int i=10;
 	public void addData(String data) {
 		System.out.println("the data is"+data);
+		if(i==10) {
+		System.out.println("newly added statement.....")	
+		}
 	}
     public static void main( String[] args )
     {
