2d227cf18c79e5a20d803d60df78e179ea42eb94
RamanaGithub <adari.vraman@gmail.com>
2021-07-06 12:56:04 +0530

    adding new statement in the code

--- src/main/java/mygit/com/git_demo/App.java
+++ src/main/java/mygit/com/git_demo/App.java
@@ -10,7 +10,7 @@ public class App
 		System.out.println("the data is"+data);
 		if(data.equals(Constant.Data_Type)) {
 		System.out.println("newly added statement.....from Constant class")	;	
-		System.out.println("statement added on 6th july");	;
+		System.out.println("statement added on 6th july.....for testing");	;
 		}
 	}
     public static void main( String[] args )
