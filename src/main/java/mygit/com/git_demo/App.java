3b1043b4267167cef19f8a09ea794a4c5325a6c7
RamanaGithub <31282855+RamanaGithub@users.noreply.github.com>
2021-07-06 11:16:51 +0530

    Update App.java

--- src/main/java/mygit/com/git_demo/App.java
+++ src/main/java/mygit/com/git_demo/App.java
@@ -9,7 +9,9 @@ public class App
 	public  addData(String data) {
 		System.out.println("the data is"+data);
 		if(data.equals(Constant.Data_Type)) {
-		System.out.println("newly added statement.....from Constant class")	;		}
+		System.out.println("newly added statement.....from Constant class")	;	
+		System.out.println("statement added on 6th july");	;
+		}
 	}
     public static void main( String[] args )
     {
